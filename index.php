6d273386f8a9d52ef8bb446b0acd6ea736789301
Dipu800 <dipu.cse.bubt97@gmail.com>
2022-02-03 17:19:55 +0600

    ddd

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+use App\classes\Example;
+require_once 'vendor/autoload.php';
+
+$example = new Example();
+$example->index();
+
+/*
+   3 major rules for variable
+     *** start with $ sign
+     *** A-Z,a-z,0-9,_,()
+     *** no numbers in 1st
+  **standard rules for variable Naming
+            start with small letter
+            meaningful
+            readable
+ */
+
